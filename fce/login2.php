bb13bdbb577581c854b8596bc07b0e6a4023bfcd
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-02-11 20:37:30 +0100

    edited evaluation key script

--- fce/login2.php
+++ fce/login2.php
@@ -5,27 +5,22 @@
 	if (isset($_POST['key'])){
 		//$key = filter_input(INPUT_POST, 'key', FILTER_SANITIZE_key)
 		$key = $_POST['key'];
-		$result = mysql_query("SELECT * FROM AcessKeys WHERE key = '$key'");
+		$result = mysql_query("SELECT key, key_eval_type FROM AcessKeys 
+								WHERE key = '$key'");
 		
 		if (!$result) die("Database access failed: " . mysql_error());
 		$rows = mysql_num_rows($result);
 		
-		if ($key == $row[0]) {
-			$result2 = mysql_query("SELECT evaluation_type FROM AcessKeys, Section, Evaluation
-			WHERE jey_crn = $row[0] AND key_crn = Section.crn 
-			AND Section.crn = Evaluation.crn");
-			
-			if (!$result2) die("Database access failed: " . mysql_error());
-			$rows2 = mysql_num_rows($result2);
-			
-			if ($rows2[0] == "mid") {
+		if ($rows == 1) {
+			$result2 = mysql_fetch_row($result);
+			if ($result2[1] == "mid") {
 				header("Location: mid_evaluation.php");
 			}
-			elseif ($rows2[0] == "final") {
+			elseif ($result2[1] == "final") {
 				header("Location: fin_evaluation.php");
 			}
 			else {
-				header("Location: ../index.html?err=Evaluationn Not Found");
+				header("Location: ../index.html?err=Evaluation Not Found");
         		exit();
 			}
 		}
