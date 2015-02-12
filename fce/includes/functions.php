6371ae882d33d2b0e7dff80053e6cd3c0191def9
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-02-13 00:47:58 +0100

    updated index2 and functions

--- fce/includes/functions.php
+++ fce/includes/functions.php
@@ -64,7 +64,7 @@ function getCurrentSemester() {
 
 //Method that checks if time to use a key has expired
 function checkSessionKeys() {
-	$now = time() //checking time when page is opened
+	$now = time(); //checking time when page is opened
 	 
 	if ($now > $_SESSION['expire']){
 		session_destroy();
