a23ba3dc337c49df0a0c428a401d482ef24670f8
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-02-12 14:46:28 +0100

    added comments and edited code for evaluation key script

--- fce/login2.php
+++ fce/login2.php
@@ -1,32 +1,44 @@
 <?php
 	include_once 'db_connect.php';
 	
+	//This method filters html input from input may be harmful
+	function fix_string($string) {
+		return htmlentities(mysql_fix_string($string));
+	}
+	
+	//This method filters php or mysql input that may be harmful
+	function mysql_fix_string($string) {
+		if (get_magic_quotes_gpc()) {
+			$string = stripslashes($string);
+		}
+		return mysql_real_escape_string($string);
+	}
+	
 	session_start():
-	if (isset($_POST['key'])){
-		//$key = filter_input(INPUT_POST, 'key', FILTER_SANITIZE_key)
-		$key = $_POST['key'];
-		$result = mysql_query("SELECT key, key_eval_type FROM AcessKeys 
-								WHERE key = '$key'");
+	if (isset($_POST['key'])){ //Checking to see if any key has been submitted
+		$key = fix_string($_POST['key']); //calling functions that handle siffing of input
+		$get_key = mysql_query("SELECT key, key_eval_type, key_crn FROM AcessKeys 
+								WHERE key = '$key'"); //Mysql query to determine if the key exists
 		
-		if (!$result) die("Database access failed: " . mysql_error());
-		$rows = mysql_num_rows($result);
+		if (!$get_key) die("Database access failed: " . mysql_error()); //Message given when there is an error
+		$rows_get_key = mysql_num_rows($get_key); //Getting the number of rows returned from query
 		
-		if ($rows == 1) {
-			$result2 = mysql_fetch_row($result);
-			if ($result2[1] == "mid") {
-				header("Location: mid_evaluation.php");
+		if ($rows_get_key == 1) { //if key is found, only one row should be gotten
+			$get_key_values = mysql_fetch_row($get_key); //getting and storing the values of row returned in an array
+			$_SESSION['crn'] = $get_key_values[2]; //The class for which evaluation is to be done is stored for later use when storing evaluation
+			
+			if ($get_key_values[1] == "mid") { //check to determine evaluation type like midterm evaluation
+				header("Location: mid_evaluation.php"); //page that is returned based on check
 			}
-			elseif ($result2[1] == "final") {
-				header("Location: fin_evaluation.php");
+			elseif ($get_key_values[1] == "final") { //check to determine evaluation type like final evaluation
+				header("Location: fin_evaluation.php"); //page that is returned based on check
 			}
 			else {
-				header("Location: ../index.html?err=Evaluation Not Found");
-        		exit();
+				header("Location: ../index.html?err=Evaluation Not Found"); //Error given when tan unknown evaluation type is gotten
 			}
 		}
 		else {
-			header('Location: ../index.html?err=Key is incorrect');
-			exit();
+			header('Location: ../index.html?err=Key is incorrect'); //Error when the key is not found in the database
 		}
 	}
 ?>
