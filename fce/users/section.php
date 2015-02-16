36c5dffb3de02287e76e4ebca6d923a3877c9fea
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-02-16 16:13:47 +0100

    restricted access to section

--- fce/users/section.php
+++ fce/users/section.php
@@ -4,6 +4,13 @@ include_once '../includes/functions.php';
 
 checkUser("secretary");
 
+$crn = $_GET['crn'];
+$status = $mysqli->query("SELECT locked FROM section WHERE crn='$crn'")->fetch_assoc();
+
+if ((!isset($_GET['crn'])) || ($status['locked']) == 1) {
+	header("Location: secretary.php");
+}
+
 ?>
 <!DOCTYPE HTML>
 <html>
