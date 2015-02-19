96eab38cdbdfe8bb150745b056ac9de66ae05a6a
BrainMaestro <ezimaestro@gmail.com>
2015-02-19 22:35:02 +0100

    Fixed faculty

--- fce/users/faculty.php
+++ fce/users/faculty.php
@@ -3,30 +3,6 @@ include_once '../includes/db_connect.php';
 include_once '../includes/functions.php';
 
 checkUser("faculty");
-
-if (isset($_POST['sbmt_semester'])) {
-    $semester = $_POST['semester'];
-    $crn_array = array();
-    $course_code_array = array();
-    $email = $_SESSION['email']; 
-    $_SESSION['eval'] = $eval_type = $_POST['eval_type'];
-    if ($eval_type == 'mid') {
-        $result = $mysqli->query("SELECT crn, course_code from section where faculty_email='$email' and semester ='$semester' and mid_evaluation = 1");
-    }  else {
-        $result = $mysqli->query("SELECT crn, course_code from section where faculty_email='$email' and semester ='$semester' and final_evaluation = 1");
-    }
-     
-    for($i = 0; $i < $result->num_rows; $i++) {
-        $row = $result->fetch_assoc();
-        array_push($crn_array, $row['crn']);
-        array_push($course_code_array, $row['course_code']);
-    }
-}
-    if (isset($_POST['submit'])) {
-        $eval = $_SESSION['eval'];
-        header("Location: $eval" . "_report.php?crn=$_POST[crn]");
-        
-    }
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -78,7 +54,7 @@ if (isset($_POST['sbmt_semester'])) {
 <div class="container">
 	<div class="row header">
 		<div class="logo navbar-left">
-			<h1><a href="../index.php">Faculty Course Evaluation</a></h1>
+			<>Faculty Course Evaluation</a></h1>
 		</div>
 		<div class="h_search navbar-right">
 			<form action="../includes/logout.php" method="post">
@@ -120,78 +96,40 @@ if (isset($_POST['sbmt_semester'])) {
         <div class="main row para"> 
             <div class="col-xs-4 text-center"></div>        
             <div class="col-xs-4 text-center border">
-				<?php 
-                if (!(isset($_POST['sbmt_semester']))) {
-                ?>
-    				<form action="" method="post">
-                        Please select semester and evaluation<br><br>
-                        <?php
+				<form action="" method="post">
+                    Please select semester<br><br>
+                    <?php
 
-                        echo '<select name="semester" class="input-sm" required>';
-                        echo '<option selected value="">--Choose Semester--</option>';
-                        $result = $mysqli->query("SELECT semester from semester");
-                        for($i = 0; $i < $result->num_rows; $i++) {
-                            $row = $result->fetch_assoc();
-                            echo "<option value='$row[semester]'>$row[semester]</option>";
-                        }
-                        echo '</select>';
-                        ?>
-
-                       <div style="height:25px"></div>
-                        <select name="eval_type" class="input-sm" required>
-                            <option selected value="">--Choose Evaluation Type--</option>
-                            <option value="mid">Midterm</option>
-                            <option value="final">Final</option>
-                        </select>
-                        <br><br>
-                        <button class="black-btn" name='sbmt_semester'>SUBMIT</button>
-                    </form>
-                <?php
-                }
-                ?>
-
-                <?php 
-                if (isset($_POST['sbmt_semester'])) {
-                ?>
-                    <form action="" method="post">
-
-                        <?php
-    					echo '<select name="crn" class="input-sm" required>';
-                        echo "<option selected value=''>--Choose course--</option>";
-                        $i = 0;
-                        while($i < count($course_code_array)) {
-    	                    echo "<option value='$crn_array[$i]'>$course_code_array[$i] - $crn_array[$i]</option>";
-                            $i++;
-                        }
-                        echo '</select>';
-                        ?> 
-    					<br><br>
-                        <button class="black-btn" name='submit'>SUBMIT</button>
-    				</form>
-                <?php
-                }
-                ?>
+                    echo '<select name="semester" class="input-sm">';
+                    echo '<option selected value="">--Choose Semester--</option>';
+                    $result = $mysqli->query("SELECT semester from semester");
+                    for($i = 0; $i < $result->num_rows; $i++) {
+                        $row = $result->fetch_assoc();
+                        echo "<option value='$row[semester]'>$row[semester]</option>";
+                    }
+                    echo '</select><br><br>';
+                    echo '<input type="text" name="search" class="round" placeholder="Ex: AUN 101">';
+                    ?>
+                    <br><br>
+                    <button class="black-btn" name='filter'>SUBMIT</button>
+                </form>
 			</div>	
 			</div>
-                        <div class="text-center">
+            <div class="text-center">
             
                 <?php
-                // if (isset($_POST['filter'])) {
-                $result = $mysqli->query("SELECT * from section where semester = '$semester' 
-                    AND faculty_email='$_SESSION[email]'");
-
+                $search = '%';
                 if (isset($_POST['filter'])) {
 
-                    $sql = "SELECT * FROM section WHERE email='$_SESSION[email]'";
                     if (isset($_POST['semester']))
-                        $sql .= " AND semester LIKE '%$_POST[semester]%'";
+                        $semester = $_POST['semester'];
 
-                    if (isset($_POST['search'])) {
-                        $sql .= " AND course_code LIKE '%$_POST[search]%'";
-                    }
-
-                    $result = $mysqli->query($sql);
+                    if (isset($_POST['search']))
+                        $search = $_POST['search'];
                 }
+
+                $result = $mysqli->query("SELECT * from section where semester LIKE '%$semester%' 
+                    AND faculty_email='$_SESSION[email]' AND course_code LIKE '%$search%'"); 
 
                 if ($result->num_rows == 0)
                     echo "<h4 class='error'>No section matches your criteria</h4>";
@@ -199,7 +137,7 @@ if (isset($_POST['sbmt_semester'])) {
                     echo "<table width='100%' class='evaltable para dean_form not-center'>
                             <caption><h3>Reports</h3><hr></caption>
                         <thead>
-                             <th>CRN</th>
+                            <th>CRN</th>
                             <th>Course Code</th>
                             <th>Course Title</th>
                             <th>Enrolled</th>
@@ -207,8 +145,6 @@ if (isset($_POST['sbmt_semester'])) {
                             <th>Final</th>
 
                         </thead>";
-
-                    
                     
                     for($i = 0; $i < $result->num_rows; $i++) {
                         $row = $result->fetch_assoc();
@@ -217,8 +153,6 @@ if (isset($_POST['sbmt_semester'])) {
                         echo "<td>$row[course_code]</td>";
                         echo "<td>$row[course_title]</td>";
                         echo "<td>$row[enrolled]</td>";
-                        
-                        //echo "<td>$row[faculty_email]</td>";
 
                         if ($row['mid_evaluation'] == 0)
                             echo "<td>No Midterm Report</td>";
@@ -242,7 +176,7 @@ if (isset($_POST['sbmt_semester'])) {
 <FOOTER>
         <div class="footer_bg"><!-- start footer -->
             <div class="container">
-                <div class="row  footer">
+                <div class="footer">
                     <div class="copy text-center">
                         <p class="link"><span>&#169; All rights reserved | Design by&nbsp;<a href="thankyou.php#fceteam"> The FCE Team</a></span></p>
                     </div>
