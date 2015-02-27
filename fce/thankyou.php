e36a1459a07ed8ec09b97252fd0b9116b8ca2d23
SkaeX <abdulmajid.hamza@aun.edu.ng>
2015-02-28 00:48:02 +0100

    Mini Minor Change
    Fixed a bug. Not worth a commit though.

--- fce/thankyou.php
+++ fce/thankyou.php
@@ -71,7 +71,7 @@
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav">
-                            <li class='active'><a href="./index.php"><img src="../images/back.png" alt="Back to Home" style="width:18px;height:18px"></a></li>
+                            <li class='active'><a href="./index.php"><img src="./images/back.png" alt="Back to Home" style="width:18px;height:18px"></a></li>
                         </ul>
                     </div><!-- /.navbar-collapse -->
                     <!-- start soc_icons -->
