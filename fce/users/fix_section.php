e2c60c99fa6c8a5b5c37a65a782df3afbadc1c0a
BrainMaestro <ezimaestro@gmail.com>
2015-04-04 22:30:33 +0100

    Errors with sections can be fixed

--- fce/users/fix_section.php
+++ fce/users/fix_section.php
@@ -3,13 +3,15 @@ include_once '../includes/db_connect.php';
 include_once '../includes/functions.php';
 
 if (isset($_POST['submit'])) {
-
+    $mysqli->query("UPDATE sections_interface SET $_POST[error_column] = '$_POST[new_value]' WHERE crn='$_GET[crn]'");
+    header("Location: process_sections.php");
 }
+
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Process Sections</title>
+<title>Fix Section</title>
 <!-- Favicon Kini -->
         <link rel="apple-touch-icon" sizes="57x57" href="../images/favicons/apple-touch-icon-57x57.png">
         <link rel="apple-touch-icon" sizes="60x60" href="../images/favicons/apple-touch-icon-60x60.png">
@@ -98,51 +100,27 @@ if (isset($_POST['submit'])) {
     <div class="container">
         <div class="text-center">
             <br></br>
-            <a href="./process_sections.php"><button class='link-active black-btn'>Process Sections</button></a>
+            <a href="./process_sections.php"><button class='black-btn'>Process Sections</button></a>
         </div><hr>
-        <div class="main text-center para"> 
-            <form action="" method="post">
-                <?php
-                $result = $mysqli->query("SELECT * FROM sections_interface");
-                if ($result->num_rows == 0)
-                    echo "<h4 class='error'>No section matches your search criteria</h4>";
-                else {
-                    echo "<table width='100%' class='not-center evaltable'>
-                    <caption><h3>Sections to Import</h3><h5>Fix the crossed out columns</h5><hr></caption>
-                    <thead>
-                        <th>CRN</th>
-                        <th>Course Code</th>
-                        <th>Semester</th>
-                        <th>School</th>
-                        <th>Course Title</th>
-                        <th>Class Time</th>
-                        <th>Location</th>
-                        <th>Enrolled</th>
-                        <th>Instructor</th>
-                        <th>Error Message</th>
-                    </thead>
-                    <tbody>";  
-                    for($i = 0; $i < $result->num_rows; $i++) {
-                        $row = $result->fetch_assoc(); 
-                        echo "<tr>";
-                        foreach ($row as $value) {
-                            if ($value == $row['error_column'])
-                                continue;
-                            if (isset($row["$row[error_column]"]) && $value == $row["$row[error_column]"])
-                                echo "<td class='input-element'>$value</td>";
-                            else
-                                echo "<td>$value</td>";
-                        }
-                        if (isset($row["$row[error_column]"]))
-                            echo "<td><a class='black-btn' href='./fix_section.php?crn=$row[crn]'>Fix</a></td>";
-                        echo "</tr>";
-                    }
-                    echo "</tbody></table><hr>";
-                }
-                ?>
-                <button class="black-btn" name="submit">Add Sections</button>
+        <div class="main text-center row para"> 
+            <div class="col-xs-4 text-center size-before"></div>        
+            <div class="col-xs-4 text-center border size-panel">
+                <form action="" method="post">
+                    <?php
+                    $crn = $_GET['crn'];
+                    $result = $mysqli->query("SELECT error_column FROM sections_interface WHERE crn='$crn'")->fetch_assoc();
+                    $result2 = $mysqli->query("SELECT $result[error_column] FROM sections_interface WHERE crn='$crn'")->fetch_array();
+                    echo "<h4>Fix Section Details</h4><hr>";
+                    echo "<label>Previous <span class='error'>$result[error_column]</span></label><br>
+                    <input type='text' value='$result2[0]' disabled><br><br>";
+                    echo "<label>New <span class='error'>$result[error_column]</span></label><br>
+                    <input type='text' name='new_value' required><br><br>";
+                    echo "<input type='hidden' name='error_column' value='$result[error_column]'>";
+                    ?>
+                    <button class="black-btn" name="submit">Fix Section</button>
 
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 </div>
