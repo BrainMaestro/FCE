462fb5cf6671ae8b6b35dc3f7eb965cb8ab828cb
SkaeX <abdulmajid.hamza@aun.edu.ng>
2015-05-21 09:10:42 +0100

    Little changes to print page
    Trimmed trailing comma. Made some text bold.

--- fce/users/print_final.php
+++ fce/users/print_final.php
@@ -60,7 +60,7 @@ $pdf->Cell(230,4,$row['course_code'],0,1,'L');
 $pdf->Cell(135,4,'Course Title:',0,0,'R');
 $pdf->Cell(230,4,$row['course_title'],0,1,'L');
 $pdf->Cell(135,4,'Report:',0,0,'R');
-$pdf->Cell(230,4,"$term Evaluation Report.",0,1,'L');
+$pdf->Cell(230,4,"$term Evaluation Report",0,1,'L');
 $pdf->Cell(135,4,'Instructor(s):',0,0,'R');
 
 $teachers = '';
@@ -70,7 +70,7 @@ $assignment = $mysqli->query("SELECT * FROM course_assignments WHERE crn='$row[c
                     $faculty = $mysqli->query("SELECT name FROM users WHERE email='$row2[faculty_email]'")->fetch_assoc();
                     $teachers.=$faculty['name'].", "; 
                 }
-
+$teachers = rtrim($teachers, ', ');
 $pdf->Cell(230,4,$teachers,0,1,'L');
 $count_crn = $course_no;    
 $count_eval_type = $eval_type;
@@ -84,6 +84,7 @@ $pdf->Cell(135,4,'Scale:',0,0,'R');
 $pdf->Cell(230,4,'5 (excellent), 4 (very good), 3 (good), 2 (margin), 1 (poor)',0,1,'L');
 $pdf->Ln(14);
 $pdf->Cell(8,4,'',0);
+$pdf->SetFont('Courier','B',10);
 $pdf->Cell(200,4,"Course",0);
 $pdf->Cell(8,4,'1',0);
 $pdf->Cell(8,4,'2',0);
@@ -92,6 +93,7 @@ $pdf->Cell(8,4,'4',0);
 $pdf->Cell(8,4,'5',0);
 $pdf->Cell(20,4,'Average',0);
 $pdf->Ln(6);
+$pdf->SetFont('Courier','',10);
 
 $pdf->Cell(8,4,'1',0);
 $pdf->Cell(200,4,"Organization.",0);
@@ -144,6 +146,8 @@ $pdf->Cell(20,4,avg_question('q5', $course_no, $eval_type, $mysqli),0);
 $pdf->Ln(6);
 
 $pdf->Cell(8,4,'',0);
+$pdf->SetFont('Courier','B',10);
+
 $pdf->Cell(200,4,"Instructor",0);
 $pdf->Cell(8,4,'1',0);
 $pdf->Cell(8,4,'2',0);
@@ -151,6 +155,7 @@ $pdf->Cell(8,4,'3',0);
 $pdf->Cell(8,4,'4',0);
 $pdf->Cell(8,4,'5',0);
 $pdf->Cell(20,4,'Average',0);
+$pdf->SetFont('Courier','',10);
 $pdf->Ln(6);
 
 $pdf->Cell(8,4,'6',0);
@@ -224,6 +229,8 @@ $pdf->Cell(20,4,avg_question('q12', $course_no, $eval_type, $mysqli),0);
 $pdf->Ln(6);
 
 $pdf->Cell(8,4,'',0);
+$pdf->SetFont('Courier','B',10);
+
 $pdf->Cell(200,4,"Student",0);
 $pdf->Cell(8,4,'1',0);
 $pdf->Cell(8,4,'2',0);
@@ -231,6 +238,7 @@ $pdf->Cell(8,4,'3',0);
 $pdf->Cell(8,4,'4',0);
 $pdf->Cell(8,4,'5',0);
 $pdf->Cell(20,4,'Average',0);
+$pdf->SetFont('Courier','',10);
 $pdf->Ln(6);
 
 $pdf->Cell(8,4,'13',0);
@@ -294,6 +302,8 @@ $pdf->Cell(20,4,avg_question('q18', $course_no, $eval_type, $mysqli),0);
 $pdf->Ln(25);
 
 $pdf->Cell(8,4,'',0);
+$pdf->SetFont('Courier','B',10);
+
 $pdf->Cell(200,4,"Rating Averages",0);
 $pdf->Cell(8,4,'Average',0);
 $pdf->Cell(8,4,'',0);
@@ -301,6 +311,7 @@ $pdf->Cell(8,4,'',0);
 $pdf->Cell(8,4,'',0);
 $pdf->Cell(8,4,'',0);
 $pdf->Cell(20,4,'',0);
+$pdf->SetFont('Courier','',10);
 $pdf->Ln(6);
 
 $pdf->Cell(8,4,'',0);
@@ -331,8 +342,10 @@ $pdf->Cell(8,4,'',0);
 $pdf->Cell(20,4,'',0);
 $pdf->Ln();
 $pdf->Cell(8,4,'',0);
+$pdf->SetFont('Courier','B',10);
 $pdf->Cell(200,4,"Total Average",0);
 $pdf->Cell(8,4,avg_final($course_no, $eval_type, $mysqli),0);
+$pdf->SetFont('Courier','',10);
 $pdf->Cell(8,4,'',0);
 $pdf->Cell(8,4,'',0);
 $pdf->Cell(8,4,'',0);
