55eca2ad35259323ddd8df36095af87174846b74
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-10-17 15:06:47 +0100

    Added Folder and Files
    * Folders: Models, Transformers, Repositories, Validators
    * Added Fractal Manager
    * Moved User Model to Models Folder

---
+++ app/Providers/Fractal.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cheezzy Tenorz
+ * Date: 10/17/2015
+ * Time: 2:38 PM
+ */
+
+namespace App\Providers;
+
+
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
+use Illuminate\Pagination\AbstractPaginator;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+
+class Fractal
+{
+    public function __construct(Manager $fractal)
+    {
+        $this->fractal = $fractal;
+        // @todo: Check this out and see if we can make it safer, used to embed relations
+        // $this->fractal->setRecursionLimit(explode(',', Input::get('embed')));
+    }
+
+    public function respondWithItem($item, $callback)
+    {
+        $resource = new Item($item, $callback);
+        $rootScope = $this->fractal->createData($resource);
+
+        return $rootScope;
+    }
+
+    public function respondWithCollection($collection, $callback)
+    {
+        $resource = new Collection($collection, $callback);
+        $rootScope = $this->fractal->createData($resource);
+
+        return $rootScope;
+    }
+
+    public function respondWithPaginatedCollection(AbstractPaginator $paginator, $callback)
+    {
+        $resource = new Collection($paginator->getCollection(), $callback);
+        $resource->setPaginator(new IlluminatePaginatorAdapter($paginator));
+        $rootScope = $this->fractal->createData($resource);
+
+        return $rootScope;
+    }
+}
