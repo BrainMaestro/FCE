53c793d2e43e857f779f40ca478be30746cc85e1
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-10-18 20:16:01 +0100

    Changed Role Table data_type to enum

--- database/migrations/2015_10_18_105040_create_roles_table.php
+++ database/migrations/2015_10_18_105040_create_roles_table.php
@@ -18,8 +18,8 @@ class CreateRolesTable extends Migration
             $table->collate = 'utf8_unicode_ci';
 
             $table->increments('id')->unsigned();
-            $table->string('role');
-            $table->string('display_name');
+            $table->enum('role', ['admin', 'dean', 'executive', 'faculty', 'secretary', 'helper']);
+            $table->enum('display_name', ['Administrator', 'Dean', 'Executive', 'Faculty', 'Secretary', 'Helper']);
             $table->timestamps();
             $table->softDeletes();
         });
