52d7d7508f19ee0e74e6a7f0bcb166bba8729821
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-10-19 09:37:33 +0100

    Removed unnecessary files

--- Extraction/clean_script.sql
+++
@@ -1,4 +0,0 @@
-SET @semester = 'Spring 2015';
-UPDATE sections SET locked='1', mid_evaluation='0', final_evaluation='0' WHERE semester=@semester;
-DELETE FROM accesskeys WHERE key_crn IN (SELECT crn FROM sections WHERE semester=@semester);
-DELETE FROM evaluations WHERE crn IN (SELECT crn FROM sections WHERE semester=@semester);
