294f54e94eac56240e5bc389b05d295acd5f4774
BrainMaestro <brainmaestro@outlook.com>
2015-10-21 21:45:29 +0100

    Added model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,70 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'password' => bcrypt('password'),
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\Role::class, function (Faker\Generator $faker) {
+    $role = $faker->randomElement(['admin', 'dean', 'executive', 'faculty', 'secretary']);
+    return [
+        'role' => $role,
+        'display_name' => ucfirst($role),
+    ];
+});
+
+$factory->define(App\Models\QuestionSet::class, function (Faker\Generator $faker) {
+    return [
+        'category' => $faker->word,
+        'title' => $faker->sentence,
+        'description' => $faker->sentence(10),
+    ];
+});
+
+$factory->define(App\Models\Semester::class, function (Faker\Generator $faker) {
+    return [
+        'semester' => $faker->word,
+    ];
+});
+
+$factory->define(App\Models\School::class, function (Faker\Generator $faker) {
+    return [
+        'school' => $faker->word,
+        'description' => $faker->sentence(10),
+    ];
+});
+
+$factory->define(App\Models\Question::class, function (Faker\Generator $faker) {
+    return [
+        'category' => $faker->word,
+        'title' => $faker->sentence,
+        'description' => $faker->sentence(10),
+    ];
+});
+
+$factory->define(App\Models\Section::class, function (Faker\Generator $faker) {
+    return [
+        'crn' => $faker->randomNumber(7),
+        'course_code' => $faker->word,
+        'semester_id' => 1,
+        'school_id' => 1,
+        'course_title' => $faker->sentence,
+        'class_time' => $faker->time(),
+        'location' => $faker->sentence,
+        'status' => 'Locked',
+        'enrolled' => $faker->randomNumber(2),
+    ];
+});
+
+$factory->define(App\Models\Evaluation::class, function (Faker\Generator $faker) {
+    return [
+        'section_id' => 1,
+        'question_id' => 1,
+    ];
+});
+
