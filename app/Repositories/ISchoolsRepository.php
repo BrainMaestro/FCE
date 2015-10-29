5ad5183b59eabd3ad6affbbb535518362d8c99ab
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-10-29 20:38:16 +0100

    Added School Repository and Interface
    * Placeholder for some methods

---
+++ app/Repositories/ISchoolsRepository.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cheezzy Tenorz
+ * Date: 10/29/2015
+ * Time: 8:30 PM
+ */
+
+namespace Fce\Repositories;
+
+interface ISchoolsRepository
+{
+    public function getSchools($data);
+
+    public function getSchoolById($school_id);
+
+    public function createSchool($data);
+
+    public function updateSchoolById($school_id);
+
+}
