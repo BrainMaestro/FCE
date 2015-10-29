ace598579737769ba343338600c8e669f25e58ae
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-10-29 20:11:10 +0100

    Added Section Repository and Interface
    * Placeholders for some methods

---
+++ app/Repositories/SectionsRepository.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cheezzy Tenorz
+ * Date: 10/29/2015
+ * Time: 8:04 PM
+ */
+
+namespace app\Repositories;
+
+use Fce\Models\Evaluation;
+use Fce\Models\Question;
+use Fce\Models\QuestionSet;
+use Fce\Models\Section;
+use Fce\Repositories\AbstractRepository;
+
+class SectionsRepository extends AbstractRepository implements ISectionsRepository
+{
+    protected $section_model;
+    protected $evaluation_model;
+    protected $question_set_model;
+    protected $question_model;
+
+    public function __construct(Section $section, Evaluation $evaluation, QuestionSet $questionSet, Question $question)
+    {
+        $this->section_model = $section;
+        $this->evaluation_model = $evaluation;
+        $this->question_set_model = $questionSet;
+        $this->question_model = $question;
+    }
+
+    public function getSections($data)
+    {
+        try {
+            Paginator::currentPageResolver(
+                function () use ($data) {
+                    return $data['offset'];
+                }
+            );
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function getSectionById($section_id)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function createSection($data)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function updateSection($data)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function getSectionEvaluationBySectionId($section_id)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function getSectionEvaluationByQuestionSet($question_set_id)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
