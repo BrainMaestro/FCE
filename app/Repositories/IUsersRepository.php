7104270579faec18eedaa8066b8a066fd6f4adc7
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-10-29 19:59:26 +0100

    Added User Repository and Interface
    * Placeholder for some methods

---
+++ app/Repositories/IUsersRepository.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cheezzy Tenorz
+ * Date: 10/29/2015
+ * Time: 7:47 PM
+ */
+
+namespace app\Repositories;
+
+interface IUsersRepository
+{
+    public function getUsers($data);
+
+    public function getUserById($user_id);
+
+    public function getUserBySectionId($section_id);
+
+    public function createUser($data);
+
+    public function deleteUserById($user_id);
+
+    public function updateUser($data);
+}
---
+++ app/Repositories/UsersRepository.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cheezzy Tenorz
+ * Date: 10/29/2015
+ * Time: 7:51 PM
+ */
+
+namespace app\Repositories;
+
+use Fce\Models\User;
+use Fce\Repositories\AbstractRepository;
+use Illuminate\Pagination\Paginator;
+
+class UsersRepository extends AbstractRepository implements IUsersRepository
+{
+    protected $user_model;
+
+    public function __construct(User $user)
+    {
+        $this->user_model = $user;
+    }
+
+    public function getUsers($data)
+    {
+        try {
+            Paginator::currentPageResolver(
+                function () use ($data) {
+                    return $data['offset'];
+                }
+            );
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function getUserById($user_id)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function getUserBySectionId($section_id)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function createUser($data)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function deleteUserById($user_id)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    public function updateUser($data)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+}
