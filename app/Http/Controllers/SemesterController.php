f805250ef143f67db95b192351bb7e9fd24f3d66
tenorz007 <chizzy.alaedu@aun.edu.ng>
2015-10-31 20:02:49 +0100

    Added Semester Controller

---
+++ app/Http/Controllers/SemesterController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Fce\Http\Controllers;
+
+use Fce\Repositories\SemestersRepository;
+use Illuminate\Http\Request;
+
+class SemesterController extends Controller
+{
+    protected $repository;
+
+    public function __construct(Request $request, SemestersRepository $semestersRepository)
+    {
+        $this->repository = $semestersRepository;
+        parent::__construct($request);
+    }
+
+    public function index()
+    {
+        try {
+            $fields['query'] = Input::get('query', null);
+            $fields['sort'] = Input::get('sort', 'created_at');
+            $fields['order'] = Input::get('order', 'ASC');
+            $fields['limit'] = Input::get('limit', 10);
+            $fields['offset'] = Input::get('offset', 1);
+
+
+        } catch (\Exception $e) {
+            return $this->errorInternalError($e->getMessage());
+        }
+    }
+
+    public function showCurrentSemeser()
+    {
+        try {
+
+        } catch (\Exception $e) {
+            return $this->errorInternalError($e->getMessage());
+        }
+    }
+
+    public function create()
+    {
+        try {
+
+        } catch (\Exception $e) {
+            return $this->errorInternalError($e->getMessage());
+        }
+    }
+
+    public function update($id)
+    {
+        try {
+
+        } catch (\Exception $e) {
+            return $this->errorInternalError($e->getMessage());
+        }
+    }
+}
