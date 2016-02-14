10dda7f17be19088a168fc32985524e70e3b6669
BrainMaestro <brainmaestro@outlook.com>
2016-02-14 15:59:50 +0100

    Improved abstract repository class methods

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -9,15 +9,19 @@
 namespace Fce\Repositories;
 
 use Fce\Providers\Fractal;
-use League\Fractal\Manager as FractalManager;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Input;
+use League\Fractal\Manager as FractalManager;
 
 abstract class Repository
 {
     /**
      * The model registered on the repository.
      *
-     * @var \Illuminate\Database\Eloquent\Model
+     * @var Model
      */
     protected $model;
 
@@ -40,7 +44,7 @@ abstract class Repository
     /**
      * Get an instance of the registered model
      *
-     * @return \Illuminate\Database\Eloquent\Model
+     * @return Model
      */
     abstract protected function getModel();
 
@@ -56,10 +60,15 @@ abstract class Repository
      *
      * @param array $attributes
      * @return static
+     * @throws \InvalidArgumentException
      */
-    protected function create(array $attributes = [])
+    protected function create(array $attributes)
     {
-        return $this->model->create($attributes);
+        try {
+            return $this->model->create($attributes);
+        } catch(\Exception $e) {
+            throw new \InvalidArgumentException('Wrong or incomplete set of attributes provided');
+        }
     }
 
     /**
@@ -71,11 +80,11 @@ abstract class Repository
      */
     protected function find($id, array $columns = ['*'])
     {
-        return $this->model->findOrFail($id, $columns);
+        return self::transform($this->model->findOrFail($id, $columns));
     }
 
     /**
-     * Finds and paginates models by the specified field and value or returns all values if specified
+     * Finds and returns one, all or a paginated list of the models with the specified field and value
      *
      * @param $field
      * @param $value
@@ -84,16 +93,45 @@ abstract class Repository
      * @param array $columns
      * @param array $with
      * @return mixed
+     * @throws \InvalidArgumentException
+     * @throws ModelNotFoundException
      */
     protected function findBy($field, $value, $limit = 15, $page = 1, array $columns = ['*'], array $with = [])
     {
-        $items =  $this->model->where($field, 'like', '%' . $value . '%')->with($with);
+        // Allows the calling method to pass multiple fields and values
+        if (is_array($field) && is_array($value)) {
+            if (count($field) != count($value)) {
+                throw new \InvalidArgumentException('Number of specified fields and values do not match');
+            }
 
-        if ($limit == 'all') {
-            return $items->get($columns);
+            $items = $this->model;
+
+            for ($i = 0; $i < count($field); $i++) {
+                $items = $items->where($field[$i], 'like', '%' . $value[$i] . '%')->with($with);
+            }
+        } else {
+            $items = $this->model->where($field, 'like', '%' . $value . '%')->with($with);
         }
 
-        return $items->paginate($limit, $columns, 'page', $page);
+        // Returns one, all or a paginated list of items
+        switch ($limit) {
+            case 'one':
+                $items = $items->first($columns);
+                break;
+
+            case 'all':
+                $items = $items->get($columns);
+                break;
+
+            default:
+                $items = $items->paginate($limit, $columns, 'page', $page);
+        }
+
+        if (is_null($items) || !count($items)) {
+            throw new ModelNotFoundException('Could not find the specified model');
+        }
+
+        return self::transform($items);
     }
 
     /**
@@ -105,6 +143,13 @@ abstract class Repository
      */
     protected function update($id, array $attributes)
     {
+        // Allows the calling method to pass the model directly if it has already been found
+        // to prevent another database lookup. This might be removed eventually
+        if ($id instanceof Model) {
+            $model = $id;
+            return $model->update($attributes);
+        }
+
         return $this->model->findOrFail($id)->update($attributes);
     }
 
@@ -132,7 +177,7 @@ abstract class Repository
      * @return mixed
      * @throws \Exception
      */
-    final protected static function transform($dataModel)
+    final public static function transform($dataModel)
     {
         $method = self::getTransformMethod($dataModel);
 
@@ -144,22 +189,18 @@ abstract class Repository
      *
      * @param $dataModel
      * @return string
-     * @throws \Exception
+     * @throws \InvalidArgumentException
      */
     final protected static function getTransformMethod($dataModel)
     {
-        switch (get_class($dataModel)) {
-            case \Illuminate\Database\Eloquent\Collection::class:
-                return 'respondWithCollection';
-
-            case \Illuminate\Pagination\LengthAwarePaginator::class:
-                return 'respondWithPaginatedCollection';
-
-            case \Illuminate\Database\Eloquent\Model::class:
-                return 'respondWithItem';
-
-            default:
-                throw new \InvalidArgumentException('Could not transform provided model');
+        if ($dataModel instanceof Collection) {
+            return 'respondWithCollection';
+        } elseif ($dataModel instanceof LengthAwarePaginator) {
+            return 'respondWithPaginatedCollection';
+        } elseif ($dataModel instanceof Model) {
+            return 'respondWithItem';
+        } else {
+            throw new \InvalidArgumentException('Could not transform provided model');
         }
     }
 }
