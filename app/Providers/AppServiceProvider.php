9242547244aa95e3fe5dc05530a75fe4ead0cc05
BrainMaestro <brainmaestro@outlook.com>
2016-03-04 21:13:52 +0100

    Registers implementations for the repository contracts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Fce\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->registerRepositories('Database', 'Eloquent');
+    }
+
+    /**
+     * Registers the repositories according to the specified namespace and type
+     *
+     * @param string $namespace
+     * @param string $type
+     */
+    public function registerRepositories($namespace, $type)
+    {
+        $prefix = 'Fce\Repositories\\';
+
+        $contracts = File::allFiles(__DIR__ . '/../Repositories/Contracts');
+
+        foreach ($contracts as $contract) {
+            $contract = str_replace('.php', '', $contract->getFileName());
+
+            $this->app->bind(
+                $prefix . 'Contracts\\' . $contract,
+                $prefix . $namespace . '\\' . $type . $contract
+            );
+        }
+
     }
 }
