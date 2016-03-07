b3c7fc0b404cbac15465527dbe5702752c0620dd
BrainMaestro <brainmaestro@outlook.com>
2016-03-07 11:31:25 +0100

    Refactored inputParameters test
    Makes it clearer what is happening.

--- tests/unit/Repositories/Database/EloquentSemesterRepositoryTest.php
+++ tests/unit/Repositories/Database/EloquentSemesterRepositoryTest.php
@@ -32,26 +32,35 @@ class EloquentSemesterRepositoryTest extends TestCase
 
     public function testInputParameters()
     {
-        $createdSemesters = factory(Fce\Models\Semester::class, 2)->create();
-        $createdSemesters = array_merge(
-            [$this->repository->transform($this->semester)['data']],
-            $this->repository->transform($createdSemesters)['data']
-        );
+        $semestersYear = factory(Fce\Models\Semester::class, 5)->create([
+            'year' => 2016
+        ]);
+        $semestersYear = $this->repository->transform($semestersYear)['data'];
 
-        $inputParameters = [
-            'query' => "season:=" . $createdSemesters[1]['season'] . "|year:=" . $createdSemesters[1]['year'],
-            'limit' => 1,
-            'page' => 1
-        ];
-
-        Input::merge($inputParameters);
+        Input::merge([
+            'query' => "year:=" . $semestersYear[0]['year'],
+        ]);
         $semesters = $this->repository->getSemesters();
 
-        $this->assertCount(1, $semesters['data']);
-        $this->assertEquals($createdSemesters[1], $semesters['data'][0]);
-        $this->assertEquals($inputParameters['limit'], $semesters['meta']['pagination']['per_page']);
-        $this->assertEquals($inputParameters['page'], $semesters['meta']['pagination']['current_page']);
-        $this->assertEquals(1, $semesters['meta']['pagination']['total']);
+        $this->assertCount(count($semestersYear), $semesters['data']);
+        $this->assertEquals($semestersYear, $semesters['data']);
+        $this->assertEquals(count($semestersYear), $semesters['meta']['pagination']['total']);
+
+        $semestersSeason = factory(Fce\Models\Semester::class, 5)->create([
+            'season' => 'Spring'
+        ]);
+        $semestersSeason = $this->repository->transform($semestersSeason)['data'];
+
+        Input::merge([
+            'query' => "season:=" . $semestersSeason[0]['season'],
+        ]);
+        $semesters = $this->repository->getSemesters();
+
+        $this->assertCount(count($semestersSeason), $semesters['data']);
+        $this->assertEquals($semestersSeason, $semesters['data']);
+        $this->assertEquals(count($semestersSeason), $semesters['meta']['pagination']['total']);
+
+        $this->assertNotEquals($semestersYear, $semestersSeason);
     }
 
     public function testGetSemesters()
