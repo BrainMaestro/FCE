c482aacaad8078629ac0e5d90b982a919f823557
BrainMaestro <brainmaestro@outlook.com>
2016-04-19 12:11:49 +0100

    Add basic layout
    Added placeholder header, footer and login

---
+++ resources/assets/js/components/app-footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>
+                    The source code is licensed is licensed
+                    <a href="http://creativecommons.org/licenses/by-nc-nd/3.0/">CC BY-NC-ND 3.0</a>.<br>
+                    And can be found at
+                    <a class="icon" href="https://github.com/BrainMaestro/FCE">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </p>
+                <p>Take a look at the code. You might learn something or better yet, teach us something new. </p>
+                <img src="http://emojione.com/wp-content/uploads/assets/emojis/1f60e.svg" alt="Happy Face" height="30" width="30">
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style>
+    .footer {
+        background: #32074b;
+        color: white;
+        padding-bottom: 2em;
+    }
+</style>
