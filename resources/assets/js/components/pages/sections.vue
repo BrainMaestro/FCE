75e959c79640a5f2df8d11531f732faffce6a73e
BrainMaestro <brainmaestro@outlook.com>
2016-04-22 14:00:21 +0100

    Extract table to separate element
    Handles array keys and 'status' elements differently

--- resources/assets/js/components/pages/sections.vue
+++ resources/assets/js/components/pages/sections.vue
@@ -1,34 +1,18 @@
 <template>
-    <table class="table is-striped">
-        <thead>
-        <tr>
-            <th v-for="column in columns">{{ column }}</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="section in sections">
-            <td v-for="item in section">
-                <!--If item is the users array. Need to find a better conditional-->
-                <span v-if="typeof item == 'object'">
-                    <span class="tag is-dark" v-for="user in item">
-                        {{ user.name }}
-                    </span>
-                </span>
-
-                <span v-else>
-                    {{ item }}
-                </span>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+    <bulma-table :columns="columns" :rows="sections">
+        <span></span>
+    </bulma-table>
 </template>
 
 <script>
     import sectionStore from '../../stores/section';
-    import { section as columns } from '../../utils/table-columns'
+    import { section as columns } from '../../utils/table-columns';
+
+    import BulmaTable from '../shared/table.vue';
 
     export default {
+        components: { BulmaTable },
+
         data() {
             return {
                 columns,
@@ -37,42 +21,38 @@
         },
 
         created() {
-            // A bit all over the place.
-            // Formats the data properly.
             sectionStore.getAllSections((res) => {
-                this.sections = res.data.map((section) => {
-                    return Object.keys(this.columns).map((key) => {
-                        const value = section[key];
-
-                        switch (key) {
-                            case 'semester':
-                                return `${value.data.season} ${value.data.year}`;
-
-                            case 'school':
-                                return value.data.school;
-
-                            case 'users':
-                                return value.data;
-
-                            default:
-                                return value;
-                        }
-                    });
-                });
+                this.sections = res.data.map(this.formatSection);
             });
         },
 
         methods: {
+            // Format the sections properly for display.
+            formatSection(section) {
+                return Object.keys(this.columns).map((key) => {
+                    const value = section[key];
+                    let item = {};
+
+                    switch (key) {
+                        case 'semester':
+                            item[key] = `${value.data.season} ${value.data.year}`;
+                            break;
+
+                        case 'school':
+                            item[key] = value.data.school;
+                            break;
+
+                        case 'users':
+                            item[key] = value.data.map(user => user.name);
+                            break;
+
+                        default:
+                            item[key] = value;
+                    }
+
+                    return item;
+                });
+            }
         }
     }
 </script>
-
-<style scoped>
-    .tag {
-        margin-bottom: 0.25em;
-    }
-
-    tr:hover {
-        background-color: inherit;
-    }
-</style>
